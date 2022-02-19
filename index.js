17f8cc4f9575e7b997b23703f7af26ef4f9faa08
HartMason <hartmason85@gmail.com>
2022-02-19 16:14:43 -0600

    last commit

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 
 const express = require('express')
-const app = express()
-const port = process.env.PORT || 4000
 
+const app = express()
+
+
+const port = process.env.PORT || 4000
+app.use(express.json()) 
 const { users } = require('./state')
+
+const counter = users.length + 1; 
+
 
 /* BEGIN - create routes here */
 app.get("/users", (req, res) => {
@@ -11,7 +17,7 @@ app.get("/users", (req, res) => {
   res.json(users)
 })  
 
-|
+
  
 app.get("/users/:id", (req, res) => {
   const userID = users.find(user => user._id == req.params.id) 
@@ -24,23 +30,32 @@ app.get("/users/:id", (req, res) => {
  
 app.post("/users", (req, res) => { 
   const newUser = {
-    "_id": 22,
+    "_id": counter,
     "name": "Batman",
     "occupation": "Crime Fighter",
     "avatar": "https://pbs.twimg.com/profile_images/718881904834056192/WnMTb__R.jpg"
 }
-  users.unshift(newUser)
-  res.json(users)
+  // users.unshift(newUser)
+  // res.json(users)
+  const newData = [...users, newUser];
+
+  res.json(newData)
+
 });
 
-app.put("/users/1", (req, res) => {
-  users[0].name = "Robin"
-  res.json(users)
+app.put("/users/:userId", (req, res) => {  
+  const userId = req.params.userId
+  const user = users.find(user => user._id == userId) //user is callback method
+  user.name = "John"
+  res.json(user)
 })
 
-app.delete("/users/1", (req, res) => {
-  users.shift()
-  res.json(users)
+app.delete(`/users/:userId`, (req, res) => { 
+  const user = users.find(user => user._id == req.params.userId) 
+  user.isActive = "offline"
+  console.log(user)
+  // users.shift()
+  res.send("deleted")
 })
 
 
